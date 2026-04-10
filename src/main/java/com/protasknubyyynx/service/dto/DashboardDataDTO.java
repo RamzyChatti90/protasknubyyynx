ccb45b8224fee5233bd967df7383c03736f6b5e1
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-10 21:47:08 +0200

    [FORGE_DEV] YC-PROTAS-S1-003: src/main/java/com/protasknubyyynx/service/dto/DashboardDataDTO.java

---
+++ src/main/java/com/protasknubyyynx/service/dto/DashboardDataDTO.java
@@ -0,0 +1,47 @@
+package com.protasknubyyynx.service.dto;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Objects;
+
+public class DashboardDataDTO implements Serializable {
+
+    private Map<String, Long> tasksByStatus;
+    private Map<String, Long> tasksByPriority;
+
+    public Map<String, Long> getTasksByStatus() {
+        return tasksByStatus;
+    }
+
+    public void setTasksByStatus(Map<String, Long> tasksByStatus) {
+        this.tasksByStatus = tasksByStatus;
+    }
+
+    public Map<String, Long> getTasksByPriority() {
+        return tasksByPriority;
+    }
+
+    public void setTasksByPriority(Map<String, Long> tasksByPriority) {
+        this.tasksByPriority = tasksByPriority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DashboardDataDTO that = (DashboardDataDTO) o;
+        return (
+            Objects.equals(tasksByStatus, that.tasksByStatus) && Objects.equals(tasksByPriority, that.tasksByPriority)
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tasksByStatus, tasksByPriority);
+    }
+
+    @Override
+    public String toString() {
+        return "DashboardDataDTO{" + "tasksByStatus=" + tasksByStatus + ", tasksByPriority=" + tasksByPriority + '}';
+    }
+}
