28f6f6fd2585f7db06b996ac8044e73c91fafdb7
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-10 21:47:09 +0200

    [FORGE_DEV] YC-PROTAS-S1-003: src/main/java/com/protasknubyyynx/repository/TaskRepository.java

--- src/main/java/com/protasknubyyynx/repository/TaskRepository.java
+++ src/main/java/com/protasknubyyynx/repository/TaskRepository.java
@@ -1,7 +1,9 @@
 package com.protasknubyyynx.repository;
 
 import com.protasknubyyynx.domain.Task;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +11,11 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("SELECT s.name, COUNT(t) FROM Task t JOIN t.status s WHERE t.createdBy = :login GROUP BY s.name")
+    List<Object[]> countTasksByStatusNameAndCreatedBy(@Param("login") String login);
+
+    @Query("SELECT p.name, COUNT(t) FROM Task t JOIN t.priority p WHERE t.createdBy = :login GROUP BY p.name")
+    List<Object[]> countTasksByPriorityNameAndCreatedBy(@Param("login") String login);
+}
